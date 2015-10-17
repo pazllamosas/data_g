aef6c1364b03633f855d967e1462db417631a175
pyritewolf <jmlanghe@gmail.com>
2015-10-17 13:16:36 -0300

    Agregando nuevo look al ABM de rol

--- Visual/AerolineaFrba/Abm Rol/AgregarRol.cs
+++ Visual/AerolineaFrba/Abm Rol/AgregarRol.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace AerolineaFrba.Abm_Rol
 {
-    public partial class Form1 : Form
+    public partial class AgregarRol : Form
     {
-        public Form1()
+        public AgregarRol()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
