f15a408fe5727b83f7bbbaad10c86499b8a641f2
Paz Llamosas <pazllamosas11@hotmail.com>
2015-10-19 16:39:10 -0300

    falta aeronave y ruta

--- Sql/DATA_G.sql
+++ Sql/DATA_G.sql
@@ -13,11 +13,11 @@ GO
 IF OBJECT_ID('DATA_G.MILLAS') IS NOT NULL
 DROP TABLE DATA_G.MILLAS
 
-IF OBJECT_ID('DATA_G.COMPRADOR') IS NOT NULL
-DROP TABLE DATA_G.COMPRADOR
-
 IF OBJECT_ID('DATA_G.DEVOLUCION') IS NOT NULL
 DROP TABLE DATA_G.DEVOLUCION
+
+IF OBJECT_ID('DATA_G.COMPRADOR') IS NOT NULL
+DROP TABLE DATA_G.COMPRADOR
 
 IF OBJECT_ID('DATA_G.PAQUETE') IS NOT NULL
 DROP TABLE DATA_G.PAQUETE
@@ -147,7 +147,7 @@ CREATE TABLE DATA_G.BENEFICIOS(
 
 CREATE TABLE DATA_G.PUNTO_DE_COMPRA(
 
-	IdPuntoDeCompra int PRIMARY KEY,
+	IdPuntoDeCompra int PRIMARY KEY IDENTITY(1,1),
 
 	TipoDePunto varchar(255)
 
@@ -193,7 +193,7 @@ CREATE TABLE DATA_G.AERONAVE(
 
 CREATE TABLE DATA_G.FUERA_DE_SERVICIO(
 
-	IdFdS int PRIMARY KEY,
+	IdFdS binary PRIMARY KEY, --0: completo vida util, 1:fuera de servicio--
 
 	Matricula nvarchar(255) FOREIGN KEY REFERENCES DATA_G.AERONAVE
 
@@ -256,22 +256,25 @@ CREATE TABLE DATA_G.PAQUETE(
 
 )
 
+CREATE TABLE DATA_G.COMPRADOR(
+	Codigo_Compra numeric(18, 0) PRIMARY KEY IDENTITY(1,1),
+
+	Paquete_Codigo numeric(18,2) FOREIGN KEY REFERENCES DATA_G.PAQUETE,
+	Pasaje_Codigo numeric(18, 0) FOREIGN KEY REFERENCES DATA_G.PASAJE,
+	IdCli int FOREIGN KEY REFERENCES DATA_G.CLIENTE,
+	IdPuntoDeCompra int FOREIGN KEY REFERENCES DATA_G.PUNTO_DE_COMPRA
+
+)
 
 CREATE TABLE DATA_G.DEVOLUCION(
 
 	IdDevolucion int PRIMARY KEY,
+	FechaDevolucion datetime,
 
-	Codigo numeric(18,2) FOREIGN KEY REFERENCES DATA_G.PAQUETE,
+	Codigo_Compra numeric(18,0) FOREIGN KEY REFERENCES DATA_G.COMPRADOR,
+	Paquete_Codigo numeric(18,2) FOREIGN KEY REFERENCES DATA_G.PAQUETE,
 	Pasaje_Codigo numeric(18,0) FOREIGN KEY REFERENCES DATA_G.PASAJE
-
-)
-
-
-CREATE TABLE DATA_G.COMPRADOR(
-
-	Pasaje_Codigo numeric(18, 0) FOREIGN KEY REFERENCES DATA_G.PASAJE,
-	IdCli int FOREIGN KEY REFERENCES DATA_G.CLIENTE,
-	IdPuntoDeCompra int FOREIGN KEY REFERENCES DATA_G.PUNTO_DE_COMPRA
+	
 
 )
 
@@ -286,17 +289,12 @@ CREATE TABLE DATA_G.MILLAS(
 
 
 
-/** MIGRACION DE DATOS **/
-
-/**ROLES**/
+-------------------------------------------------- MIGRACION DE DATOS ----------------------------------------------------------
 
 INSERT INTO DATA_G.ROL(IdRol, Descripcion)
 VALUES (0,'Administrador')
 INSERT INTO DATA_G.ROL(IdRol, Descripcion)
 VALUES (1, 'Cliente')
-
-
-/** FUNCIONALIDADES **/
 
 SET IDENTITY_INSERT DATA_G.FUNCIONALIDADES ON
 INSERT INTO DATA_G.FUNCIONALIDADES(IdFuncionalidad,DescripcionFunc)
@@ -305,17 +303,12 @@ INSERT INTO DATA_G.FUNCIONALIDADES(IdFuncionalidad,DescripcionFunc)
 VALUES (1,'Modificar acceso usuario')
 INSERT INTO DATA_G.FUNCIONALIDADES(IdFuncionalidad,DescripcionFunc)
 VALUES (2,'Eliminar acceso usuario')
-
+/**seguir agregando**/
 SET IDENTITY_INSERT DATA_G.FUNCIONALIDADES OFF
-
-
-/** FUNCIONALIDADES POR ROL **/
 
 INSERT INTO DATA_G.ROL_POR_FUNCIONALIDADES
 VALUES(0,0)
-
-
-/** CLIENTE **/ 
+/**seguir agregando**/
 
 INSERT INTO DATA_G.CLIENTE(IdRol,Nombre, Apellido,Dni,Direccion,Telefono,Mail,Fecha_Nac)
 SELECT distinct 1,Cli_Nombre,Cli_Apellido, Cli_Dni,Cli_Dir,Cli_Telefono,Cli_Mail,Cli_Fecha_Nac FROM gd_esquema.Maestra 
@@ -325,99 +318,12 @@ FROM gd_esquema.Maestra ma
 where exists (select 1 
 			from gd_esquema.Maestra ma2 where ma2.Cli_Dni = ma.Cli_Dni and  ma.Cli_Apellido <> ma2.Cli_Apellido and ma.Cli_Nombre <> ma2.Cli_Nombre)
 **/
+
 INSERT INTO DATA_G.CLIENTE(IdRol,Nombre, Apellido,Dni,Direccion,Telefono,Mail,Fecha_Nac,Username,Password)
 VALUES(0,'Maria','Lopez',33652149,'J.C Paz 520',47921563,'mlopez@gmail.com', '1990-05-21','mlopez','w23e')
 INSERT INTO DATA_G.CLIENTE(IdRol,Nombre, Apellido,Dni,Direccion,Telefono,Mail,Fecha_Nac,Username,Password)
 VALUES(0,'Administrador General',' ',32652149,'J.C Paz 720',47971573,'admin@gmail.com', '1990-05-11','admin','w23e')
 
-
-/** PASAJE  **/
-
-INSERT INTO DATA_G.PASAJE(Pasaje_Codigo,Precio,FechaCompra, CantMillas, NroVuelo, IdCli, IdButaca, Ciudad_Origen, Ciudad_Destino)
- 
-SELECT 	M.Pasaje_Codigo,
-		M.Pasaje_Precio,
-		M.Pasaje_FechaCompra,
-		CAST(ROUND(M.Pasaje_Precio/10,1) AS decimal(10,0)) AS 'CantMillas',
-		v.NroVuelo,
-		c.idCli,
-		(b.NroButaca) AS 'Nro Butaca',
-		r.Origen,
-		r.Destino 
-	 
- 
-FROM gd_esquema.Maestra M, DATA_G.BUTACA b, DATA_G.VUELO v, DATA_G.AERONAVE a, DATA_G.CLIENTE c, DATA_G.RUTA r
-	/**WHERE M.Pasaje_Codigo <> 0
-	AND v.FechaSalida = M.FechaSalida
-	AND v.FechaLlegada = M.FechaLLegada
-	AND v.Matricula = M.Aeronave_Matricula
-	AND c.Dni = M.Cli_Dni
-	AND b.NroVuelo = v.NroVuelo
-	AND b.Piso = a.Piso
-	AND 
-	**/
-
-
-/** PAQUETE **/
-
-
-
-INSERT INTO DATA_G.PAQUETE(Codigo, FechaCompra, Precio, NroVuelo, KG)
-
-SELECT M.Paquete_Codigo,
-	   M.Paquete_FechaCompra,
-	   M.Paquete_Precio,
-	   V.NroVuelo,
-	   M.Paquete_KG
-
-FROM gd_esquema.Maestra M, DATA_G.VUELO V 
-	WHERE M.Paquete_Codigo <> 0
-
-
-INSERT INTO DATA_G.VUELO(FechaEstimadaLlegada, FechaLlegada, FechaSalida, IdRuta, Matricula)
-
-SELECT M.Fecha_Llegada_Estimada,
-	   M.FechaLlegada,
-	   M.FechaSalida,
-	   R.IdRuta,
-	   M.Aeronave_Matricula
-
-FROM gd_esquema.Maestra M, DATA_G.RUTA R
-
-/**
-INSERT INTO DATA_G.RUTA(Codigo, Precio_BaseKG, Precio_BasePasaje, Origen, Destino)
-
-SELECT DISTINCT M.Ruta_Codigo,
-	   M.Ruta_Precio_BaseKG,
-	   M.Ruta_Precio_BasePasaje,
-	   c1.Nombre AS 'Origen',
-	   c2.Nombre AS 'Destino'
-    
-FROM gd_esquema.Maestra M, DATA_G.CIUDAD c1, DATA_G.CIUDAD c2
-WHERE M.Ruta_Ciudad_Origen = c1.Nombre
-	AND M.Ruta_Ciudad_Destino = c2.Nombre
- Order by 1
- **/
-
-INSERT INTO DATA_G.BUTACA(NroButaca, Tipo, Piso, Estado)
-
-SELECT DISTINCT M.Butaca_Nro,
-				M.Butaca_Tipo,
-				M.Butaca_Piso,
-				0 AS 'Estado'
-
-
-FROM gd_esquema.Maestra M 
-WHERE M.Butaca_Tipo <> '0'
-ORDER BY 1
-
-/**INSERT INTO DATA_G.AERONAVE(Matricula, Modelo, KG_Disponibles, Fabricante, CantButacas, Piso)
-SELECT M.Aeronave_Matricula, M.Aeronave_Modelo, M.Aeronave_KG_Disponibles, M.Aeronave_Fabricante, ,
-FROM gd_esquema.Maestra M**/
-
-INSERT INTO DATA_G.CIUDAD(Nombre)
-  SELECT DISTINCT Ruta_Ciudad_Destino FROM gd_esquema.Maestra 
- 
 INSERT INTO DATA_G.PRODUCTO(Descripcion, CostoEnMillas)
 VALUES('Mochila',125)
 INSERT INTO DATA_G.PRODUCTO(Descripcion, CostoEnMillas)
@@ -441,6 +347,100 @@ VALUES('Estadia en Sao Paulo 5 dias',2000)
 INSERT INTO DATA_G.PRODUCTO(Descripcion, CostoEnMillas)
 VALUES('Viaje a Barcelona',1750) 
 
+INSERT INTO DATA_G.BENEFICIOS (IdCli, Milla)
+SELECT DISTINCT C.IdCli,
+				(SUM(P.CostoEnMillas)) AS 'MILLA'
+FROM DATA_G.CLIENTE C, DATA_G.PRODUCTO P
+GROUP BY C.IdCli
+
+SET IDENTITY_INSERT DATA_G.PUNTO_DE_COMPRA ON
+INSERT INTO DATA_G.PUNTO_DE_COMPRA(IdPuntoDeCompra,TipoDePunto)
+VALUES (1, 'Terminal Kiosco')
+INSERT INTO DATA_G.PUNTO_DE_COMPRA(IdPuntoDeCompra,TipoDePunto)
+VALUES(2, 'Oficina Comercial o Aeropuerto')
+SET IDENTITY_INSERT DATA_G.PUNTO_DE_COMPRA OFF
+
+INSERT INTO DATA_G.CIUDAD(Nombre)
+SELECT DISTINCT Ruta_Ciudad_Destino FROM gd_esquema.Maestra
+
+/**
+INSERT INTO DATA_G.RUTA(Codigo, Precio_BaseKG, Precio_BasePasaje, Origen, Destino)
+
+SELECT DISTINCT M.Ruta_Codigo,
+	   M.Ruta_Precio_BaseKG,
+	   M.Ruta_Precio_BasePasaje,
+	   c1.Nombre AS 'Origen',
+	   c2.Nombre AS 'Destino'
+    
+FROM gd_esquema.Maestra M, DATA_G.CIUDAD c1, DATA_G.CIUDAD c2
+WHERE M.Ruta_Ciudad_Origen = c1.Nombre
+	AND M.Ruta_Ciudad_Destino = c2.Nombre
+ Order by 1
+ **/
+
+ /**INSERT INTO DATA_G.AERONAVE(Matricula, Modelo, KG_Disponibles, Fabricante, CantButacas, Piso)
+SELECT M.Aeronave_Matricula, M.Aeronave_Modelo, M.Aeronave_KG_Disponibles, M.Aeronave_Fabricante, ,
+FROM gd_esquema.Maestra M**/
+
+INSERT INTO DATA_G.FUERA_DE_SERVICIO( IdFdS,Matricula)
+SELECT ( Matricula)
+FROM DATA_G.AERONAVE
+
+INSERT INTO DATA_G.VUELO(FechaEstimadaLlegada, FechaLlegada, FechaSalida, IdRuta, Matricula)
+SELECT M.Fecha_Llegada_Estimada,
+	   M.FechaLlegada,
+	   M.FechaSalida,
+	   R.IdRuta,
+	   M.Aeronave_Matricula
+FROM gd_esquema.Maestra M, DATA_G.RUTA R
+
+INSERT INTO DATA_G.BUTACA(NroButaca, Tipo, Piso, Estado)
+SELECT DISTINCT M.Butaca_Nro,
+				M.Butaca_Tipo,
+				M.Butaca_Piso,
+				0 AS 'Estado'
+FROM gd_esquema.Maestra M 
+WHERE M.Butaca_Tipo <> '0'
+ORDER BY 1
+
+INSERT INTO DATA_G.PASAJE(Pasaje_Codigo,Precio,FechaCompra, CantMillas, NroVuelo, IdCli, IdButaca, Ciudad_Origen, Ciudad_Destino)
+SELECT 	M.Pasaje_Codigo,
+		M.Pasaje_Precio,
+		M.Pasaje_FechaCompra,
+		CAST(ROUND(M.Pasaje_Precio/10,1) AS decimal(10,0)) AS 'CantMillas',
+		v.NroVuelo,
+		c.idCli,
+		(b.NroButaca) AS 'Nro Butaca',
+		r.Origen,
+		r.Destino 
+FROM gd_esquema.Maestra M, DATA_G.BUTACA b, DATA_G.VUELO v, DATA_G.AERONAVE a, DATA_G.CLIENTE c, DATA_G.RUTA r
+	/**WHERE M.Pasaje_Codigo <> 0
+	AND v.FechaSalida = M.FechaSalida
+	AND v.FechaLlegada = M.FechaLLegada
+	AND v.Matricula = M.Aeronave_Matricula
+	AND c.Dni = M.Cli_Dni
+	AND b.NroVuelo = v.NroVuelo
+	AND b.Piso = a.Piso
+	AND 
+	**/
+
+INSERT INTO DATA_G.PAQUETE(Codigo, FechaCompra, Precio, NroVuelo, KG)
+SELECT M.Paquete_Codigo,
+	   M.Paquete_FechaCompra,
+	   M.Paquete_Precio,
+	   V.NroVuelo,
+	   M.Paquete_KG
+FROM gd_esquema.Maestra M, DATA_G.VUELO V 
+WHERE M.Paquete_Codigo <> 0
+
+INSERT INTO DATA_G.COMPRADOR(Pasaje_Codigo, IdCli, Codigo_Compra, IdPuntoDeCompra)
+SELECT
+FROM DATA_G.CLIENTE C, DATA_G.PASAJE, 
+
+INSERT INTO DATA_G.DEVOLUCION(IdDevolucion, Codigo_Compra Pasaje_Codigo, Paquete_Codigo)
+SELECT
+FROM
+
 INSERT INTO DATA_G.MILLAS (IdCli, CantMillas, NroVuelo)
 SELECT DISTINCT C.IdCli,
 				P.CantMillas,
@@ -449,11 +449,7 @@ FROM DATA_G.CLIENTE C, DATA_G.VUELO V, DATA_G.PASAJE P
 
 	
 
-INSERT INTO DATA_G.BENEFICIOS (IdCli, Milla)
-SELECT DISTINCT C.IdCli,
-				(SUM(P.CostoEnMillas)) AS 'MILLA'
-FROM DATA_G.CLIENTE C, DATA_G.PRODUCTO P
-GROUP BY C.IdCli
+
 
 
 
