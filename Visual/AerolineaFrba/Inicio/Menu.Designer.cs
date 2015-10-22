eae9d79440bdb41db1aff17f375c530ef8582d95
Duilio Camps <CampsDuilio@hotmail.com>
2015-10-22 11:57:50 -0300

    hice el menu de inicio porque estaba aburrido

--- Visual/AerolineaFrba/Inicio/Menu.Designer.cs
+++ Visual/AerolineaFrba/Inicio/Menu.Designer.cs
@@ -32,6 +32,17 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.deslogearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,7 +52,7 @@
             this.archivoToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(634, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(578, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -56,15 +67,147 @@
             // deslogearToolStripMenuItem
             // 
             this.deslogearToolStripMenuItem.Name = "deslogearToolStripMenuItem";
-            this.deslogearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deslogearToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
             this.deslogearToolStripMenuItem.Text = "Salir";
             this.deslogearToolStripMenuItem.Click += new System.EventHandler(this.deslogearToolStripMenuItem_Click);
+            // 
+            // button5
+            // 
+            this.button5.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(306, 205);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(260, 35);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Ver Ciudades";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(305, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 35);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Ver Aeronaves";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(305, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 35);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Ver Roles";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(305, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 35);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Ver Rutas";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(12, 123);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(260, 35);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Canjear Millas";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(12, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(260, 35);
+            this.button6.TabIndex = 26;
+            this.button6.Text = "Comprar pasaje";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(12, 82);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(260, 35);
+            this.button7.TabIndex = 27;
+            this.button7.Text = "Consultar Millas";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.Location = new System.Drawing.Point(12, 205);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(260, 35);
+            this.button8.TabIndex = 28;
+            this.button8.Text = "Devolución";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button9.Location = new System.Drawing.Point(305, 41);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(260, 35);
+            this.button9.TabIndex = 29;
+            this.button9.Text = "Generar Viaje";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button10.Location = new System.Drawing.Point(12, 164);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(260, 35);
+            this.button10.TabIndex = 30;
+            this.button10.Text = "Registro de Llegada a Destino";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button11.Location = new System.Drawing.Point(167, 246);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(260, 35);
+            this.button11.TabIndex = 31;
+            this.button11.Text = "Top 5";
+            this.button11.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 362);
+            this.ClientSize = new System.Drawing.Size(578, 297);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -82,5 +225,16 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem deslogearToolStripMenuItem;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
     }
 }
